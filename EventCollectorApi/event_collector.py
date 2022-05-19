7dd362daebbff4cad8ad9f14423d0e4180635a6d
Daniel Angerer <daniel.angerer@me.com>
2022-05-19 10:11:07 +0200

    new file and class meetup, integration to event_collector

--- EventCollectorApi/event_collector.py
+++ EventCollectorApi/event_collector.py
@@ -1,20 +1,7 @@
 from email import message
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
-from dotenv import load_dotenv, find_dotenv
-import requests
-
-env_path = find_dotenv()
-load_dotenv(dotenv_path=env_path)
-
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-base_api_url = os.getenv('BASE_API_URL')
-redirect_uri = os.getenv('REDIRECT_URI')
-
-auth_url = os.getenv('AUTH_URL')
-access_url = os.getenv('ACCESS_URL')
+from EventCollectorApi.meetup import Meetup
 
 app = FastAPI()
 app.add_middleware(
@@ -25,62 +12,18 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+meetup = Meetup()
+meetup.get_outh_token()
+'''
+creates a new meetup api token for querying events
+'''
+
 @app.get("/")
 def index():
     return {"greeting": f"Welcome to the Sports Experience Collector API"}
 
 @app.get("/meetup")
 def query_meetup(activity, lat, lon, radius):
-    results = {}
-    grant_type = 'anonymous_code'
-    headers = {'Accept': 'application/json'}
-    auth_params = {'client_id': client_id, 'response_type': grant_type, 'redirect_uri': redirect_uri}
-    auth_response = requests.get(auth_url, params=auth_params, headers=headers)
-    auth_token = auth_response.json()["code"]
-    access_params = {'client_id': client_id,
-                        'client_secret': client_secret,
-                        'grant_type': grant_type,
-                        'redirect_uri': redirect_uri,
-                        'code': auth_token}
-    access_response = requests.post(access_url, params=access_params, headers=headers)
-    access_token = access_response.json()
-    auth_string = 'Bearer %s' % access_token
-    oauth_headers = {'Accept': 'application/json', 'Authorization': auth_string}
+    events = meetup.get_events_by_city(activity, lat, lon, radius)
 
-    query = """query {
-        keywordSearch(filter: { query: "%s", lat: %s, lon: %s, radius: %s, source: EVENTS }) {
-            count
-            pageInfo {
-                endCursor
-            }
-            edges {
-                node {
-                    id
-                }
-            }
-        }
-    }""" % (activity, lat, lon, radius)
-    res = requests.post(base_api_url, json={'query': query}, headers=oauth_headers)
-
-    for events in res.json()['data']['keywordSearch']['edges']:
-        event_id = events['node']['id']
-        query_event = """
-            query {
-                event(id: %s) {
-                    title
-                    description
-                    dateTime
-                    eventUrl
-                }
-            }
-            """ % (event_id)
-        res_event = requests.post(base_api_url, json={'query': query_event}, headers=oauth_headers)
-
-        if 'events' not in results.keys():
-            results['events'] = []
-
-        results['events'].append(res_event.json()['data']['event'])
-
-
-
-    return results
+    return events
---
+++ EventCollectorApi/meetup.py
@@ -0,0 +1,117 @@
+import os
+from EventCollectorApi.event_collector import query_meetup
+from dotenv import load_dotenv, find_dotenv
+import requests
+
+class Meetup():
+    def __init__(self):
+        '''
+        read all needed configuration parameters for oauth process from .env
+        '''
+        env_path = find_dotenv()
+        load_dotenv(dotenv_path=env_path)
+
+        self.client_id = os.getenv('CLIENT_ID')
+        self.client_secret = os.getenv('CLIENT_SECRET')
+        self.base_api_url = os.getenv('BASE_API_URL')
+        self.redirect_uri = os.getenv('REDIRECT_URI')
+
+        self.auth_url = os.getenv('AUTH_URL')
+        self.access_url = os.getenv('ACCESS_URL')
+
+    def get_outh_token(self):
+        '''
+        oauth process flow to get the token needed for querying the Meetup API with graphQL
+        '''
+        grant_type = 'anonymous_code'
+        headers = {'Accept': 'application/json'}
+        auth_params = {'client_id': self.client_id, 'response_type': grant_type, 'redirect_uri': self.redirect_uri}
+
+        try:
+            auth_response = requests.get(self.auth_url, params=auth_params, headers=headers)
+        except requests.exceptions.RequestException as error:
+            raise SystemExit(error)
+
+        auth_token = auth_response.json()["code"]
+        access_params = {'client_id': self.client_id,
+                            'client_secret': self.client_secret,
+                            'grant_type': grant_type,
+                            'redirect_uri': self.redirect_uri,
+                            'code': auth_token}
+
+        try:
+            access_response = requests.post(self.access_url, params=access_params, headers=headers)
+        except requests.exceptions.RequestException as error:
+            raise SystemExit(error)
+
+        access_token = access_response.json()
+        auth_string = 'Bearer %s' % access_token
+        oauth_headers = {'Accept': 'application/json', 'Authorization': auth_string}
+        self.oauth_headers = oauth_headers
+
+    def query_meetup (self, query):
+        try:
+            res = requests.post(self.base_api_url, json={'query': query}, headers=self.oauth_headers)
+        except requests.exceptions.RequestException as error:
+            raise SystemExit(error)
+
+        return res.json()
+
+    def get_events_by_city(self, activity, lat, lon, radius):
+        '''
+        collect the events from a city specifed by their lat and lon for a special activity
+        '''
+        self.activity = activity
+        self.lat = lat
+        self.lon = lon
+        self.radius = radius
+
+        query = '''
+            query {
+                keywordSearch(filter: { query: "%s", lat: %s, lon: %s, radius: %s, source: EVENTS }) {
+                    count
+                    pageInfo {
+                        endCursor
+                    }
+                    edges {
+                        node {
+                            result {
+                            ... on Event {
+                            title
+                            description
+                            priceTier
+                            numberOfAllowedGuests
+                            dateTime
+                            timezone
+                            eventUrl
+                            venue {
+                                name
+                                address
+                                city
+                                state
+                                postalCode
+                                country
+                                lat
+                                lng
+                            }
+                            fee
+                            }
+                        }
+                        }
+                    }
+                }
+            }
+        ''' % (self.activity, self.lat, self.lon, self.radius)
+
+        events = self.query_meetup(query)
+
+        return events
+
+def main():
+    meetup = Meetup()
+    meetup.get_outh_token()
+    events = meetup.get_events_by_city("fitness", 52.520008, 13.404954, 100)
+    print(events)
+
+if __name__ == '__main__':
+    main()
