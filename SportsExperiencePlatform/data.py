6ed8a164de7dd5e6a4f09dad030b9326884b82ff
Daniel Angerer <daniel.angerer@me.com>
2022-05-21 17:45:17 +0200

    added initial version of get data

--- SportsExperiencePlatform/data.py
+++ SportsExperiencePlatform/data.py
@@ -2,24 +2,40 @@ import psycopg2
 from dotenv import load_dotenv, find_dotenv
 import os
 import pandas as pd
-import streamlit as st
 
-env_path = find_dotenv()
-load_dotenv(env_path)
-POSTGRE_HOST = os.getenv('POSTGRE_HOST')
-POSTGRE_DATABASE = os.getenv('POSTGRE_DATABASE')
-POSTGRE_DB_USER = os.getenv('POSTGRE_DB_USER')
-POSTGRE_DB_PW = os.getenv('POSTGRE_DB_PW')
+def connect_db():
+    env_path = find_dotenv()
+    load_dotenv(env_path)
+    POSTGRE_HOST = os.getenv('POSTGRE_HOST')
+    POSTGRE_DATABASE = os.getenv('POSTGRE_DATABASE')
+    POSTGRE_DB_USER = os.getenv('POSTGRE_DB_USER')
+    POSTGRE_DB_PW = os.getenv('POSTGRE_DB_PW')
 
-conn = psycopg2.connect(
-    host=POSTGRE_HOST,
-    database=POSTGRE_DATABASE,
-    user=POSTGRE_DB_USER,
-    password=POSTGRE_DB_PW)
+    try:
+        conn = psycopg2.connect(
+        host=POSTGRE_HOST,
+        database=POSTGRE_DATABASE,
+        user=POSTGRE_DB_USER,
+        password=POSTGRE_DB_PW)
+    except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
-query = "select version()"
-df = pd.read_sql(query, conn)
+    return conn
 
-st.write(df)
+def get_data(conn):
+    query_users = "select * from users"
+    query_events = "select * from offers"
+    df_users = pd.read_sql(query_users, conn)
+    df_events = pd.read_sql(query_events, conn)
 
-conn.close()
+    conn.close()
+
+    return (df_users, df_events)
+
+def main():
+    conn = connect_db()
+    df_users, df_events = get_data(conn)
+    print(df_users.shape, df_events.shape)
+
+if __name__ == '__main__':
+    main()
