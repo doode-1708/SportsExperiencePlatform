bda0742a4833cf7f9992e5bf8ac65054b00c6767
Roberta van der Meijden <roberta.meijden@gmail.com>
2022-06-02 21:48:51 +0200

    created api folder for recommendation api

---
+++ api/fast.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+def index():
+    return {"greeting": f"Welcome to the Sports Experience Recommendation System"}
+
+@app.get('/predict')
+def predict(): #def predict(insert input instead)
+    return {"greeting": f"Welcome to the Sports Experience Recommendation System"}
