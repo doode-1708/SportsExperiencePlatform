0ba5a2ce36c00dec5a6f4154329326201c40d958
Roberta van der Meijden <roberta.meijden@gmail.com>
2022-06-04 18:13:12 +0200

    finished the base file

--- api/predict-api.py
+++ api/predict-api.py
@@ -2,8 +2,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from SportsExperiencePlatform.predict import content_recommender
-# import datetime
-# from datetime import date
+# from SportsExperiencePlatform.recommender import training (to be added)
+
 
 app = FastAPI()
 
@@ -15,30 +15,38 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+# loading cosine_sim get model from gs
+storage_location = f'{STORAGE_LOCATION}/model_cosine_sim.npz'
+blob = client.blob(storage_location)
+blob.download_to_filename('model_cosine_sim.npz')
+print("=> model downloaded from storage")
+model = load_npz('model_cosine_sim.npz')
+
+# df will work when training is added to SportsExperiencePlatform.recommender (line 4)
+# df = recommender.training.get_transformed_df()[1]
+
+
 @app.get("/")
 def index():
     return {"greeting": f"Welcome to the Sports Experience Recommendation System"}
 
 @app.get('/recommender')
 def predict(user_id):
-    return content_recommender(user_id, cosine_sim, df)
+    return content_recommender(user_id, cosine_sim = model, df)
 
 
-# (user_id, cosine_sim = cosine_sim, df = events_df)
+
 
 # ===============================================
 # ***************** NEXT STEPS: *****************
 # ===============================================
 #
-# how to load: cosine_sim:
-# get model from gs
-# storage_location = f'{STORAGE_LOCATION}/model_cosine_sim.npz'
-# blob = client.blob(storage_location)
-# blob.download_to_filename('model_cosine_sim.npz')
-# print("=> model downloaded from storage")
-# model = load_npz('model_cosine_sim.npz')
-#
-# check # data.py
-#
-# , df, title_to_index
 # test it
+
+
+# Daniel's instructions:
+# file: predict-api.py
+#  --> fastapi
+#  --> import from data google functions, get model, do the recommendation
+#  --> import predicting functions from recommender.py
+#       --> get_title, content_recommender, haversine_vectorized, get_user_loc
