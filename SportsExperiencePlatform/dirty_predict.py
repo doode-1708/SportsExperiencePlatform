49e0cc49a83fcdf26b7d392ca24d135f6b876b1d
Daniel Angerer <daniel.angerer@me.com>
2022-06-06 08:52:03 +0200

    emergency settup

--- SportsExperiencePlatform/dirty_predict.py
+++ SportsExperiencePlatform/dirty_predict.py
@@ -1,83 +1,14 @@
 from email import message
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from google.cloud import storage
-from scipy.sparse import load_npz
-from SportsExperiencePlatform.utils import haversine_vectorized
 import pandas as pd
-from datetime import date
-import datetime
 import os
+from SportsExperiencePlatform.params import BUCKET_NAME, STORAGE_LOCATION
+from google.cloud import storage
+import json
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/credentials.json"
-
-BUCKET_NAME = 'wagon-817-project-sports'
-STORAGE_LOCATION = 'models/recommender'
-
-def get_title(user_id, ahoy_events_df, events_df):
-    ''' this function grabs the title of an event used by an user from ahoy_events dataset'''
-
-    #event_idx = ahoy_events_df[ahoy_events_df['user_id']== user_id].iloc[0].properties['offer']
-    # now we get the title of the event
-    event_idx = 1375
-    title = events_df[events_df['id']==event_idx].title
-
-    return title
-
-def get_user_loc(user_id, users_df):
-    ''' this finction grabs the location of an user from ahoy_events dataset'''
-
-    users_idx = users_df[users_df['id']== user_id]
-    # now we get the user's latitude and longitude
-
-    location_dict = {'user_latitude' : float(users_idx.latitude.values),
-
-                     'user_longitude' : float(users_idx.longitude.values)
-                        }
-
-    return location_dict
-
-
-def content_recommender(user_id, cosine_sim, events_df, users_df, ahoy_events_df, title_to_index):
-    '''This recommender finds out 20 closest matches with a given event'''
-
-    #calling user id from ahoy_events
-
-    title = get_title(user_id, ahoy_events_df, events_df)
-
-    idx = title_to_index[title]
-
-    sim_scores = list(enumerate(cosine_sim[idx].todense().tolist()[0]))
-    # list of set [(index, sim score), (index, sim score), ...]
-
-    sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
-    # Sort the list of set by sim score, index with highest simlirarity
-    # Will be at the beginning of the list
-
-    sim_scores = sim_scores[1:101] # top 100 events (first one is our input events so we ignore it)
-
-    sport_indices = [i[0] for i in sim_scores] # we grab the indices of those 10 events
-
-    df_all_recommendations = events_df.iloc[sport_indices][['id','title', 'latitude', 'longitude', 'offer_date']]
-
-    #df_date_filter = df_all_recommendations.copy() #[df_all_recommendations['offer_date'] >= datetime.date.today()]
-#
-    ##calcullation of haversine distance for users:
-    #user_latitude = get_user_loc(user_id, users_df)['user_latitude']
-    #user_longitude = get_user_loc(user_id, users_df)['user_longitude']
-#
-    #df_date_filter['user_latitude'] = user_latitude
-    #df_date_filter['user_longitude'] = user_longitude
-#
-    #df_date_filter["distance"] = haversine_vectorized(df_date_filter.latitude, df_date_filter.longitude,
-    #                                                  df_date_filter.user_latitude, df_date_filter.user_longitude)
-#
-    #df_location_filter = df_date_filter[df_date_filter['distance'] <= 200.0]
-#
-#
-    return df_all_recommendations.id
-    #return df_location_filter.title
-#     return user_latitude
+if not os.uname()[1] == 'doodebook':
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/credentials.json"
 
 app = FastAPI()
 app.add_middleware(
@@ -89,32 +20,12 @@ app.add_middleware(
 )
 
 client = storage.Client().bucket(BUCKET_NAME)
-
-# get model from gs
-storage_location = f'{STORAGE_LOCATION}/model_cosine_sim.npz'
+storage_location = f'{STORAGE_LOCATION}/recommender.json'
 blob = client.blob(storage_location)
-blob.download_to_filename('model_cosine_sim.npz')
-print("=> model downloaded from storage")
-model = load_npz('model_cosine_sim.npz')
+blob.download_to_filename('recommender.json')
 
-storage_location_ahoy = f'{STORAGE_LOCATION}/ahoy_events_df.csv'
-blob = client.blob(storage_location_ahoy)
-blob.download_to_filename('ahoy_events_df.csv')
-ahoy_events_df = pd.read_csv('ahoy_events_df.csv')
-
-storage_location_events = f'{STORAGE_LOCATION}/events_df.csv'
-blob = client.blob(storage_location_events)
-blob.download_to_filename('events_df.csv')
-events_df = pd.read_csv('events_df.csv')
-
-storage_location_users = f'{STORAGE_LOCATION}/users_df.csv'
-blob = client.blob(storage_location_users)
-blob.download_to_filename('users_df.csv')
-users_df = pd.read_csv('users_df.csv')
-
-#print(ahoy_events_df[ahoy_events_df['user_id']== 59].iloc[0].properties['offer'])
-
-title_to_index = pd.Series(events_df.index, index = events_df["title"]).drop_duplicates()
+with open('recommender.json', 'r') as fp:
+    recommendation = json.load(fp)
 
 @app.get("/")
 def index():
@@ -122,8 +33,5 @@ def index():
 
 @app.get("/recommender")
 def recommender(user_id):
-    id = int(user_id)
-    results = content_recommender(id, model, events_df, users_df, ahoy_events_df, title_to_index)
 
-    #print(results.info())
-    return results.tolist()
+    return recommendation[user_id]
---
+++ SportsExperiencePlatform/utils_debug.py
@@ -0,0 +1,102 @@
+import numpy as np
+from googletrans import Translator
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+import string
+import random
+import re
+import spacy
+
+def language_translation(string):
+        translator = Translator()
+        if translator.detect(string).lang != 'en':
+            result = translator.translate(string)
+            return result.text
+        else:
+            return string
+
+def clean_string(text,stem="None"):
+    # load spacy
+    nlp = spacy.load('en_core_web_sm')
+
+    final_string = ""
+
+    # make lower
+    text = text.lower()
+
+    # remove http
+    text = re.sub(r"http.*\.[a-z]{2,3}","",text)
+
+    # remove www
+    text = re.sub(r"www.*\.[a-z]{2,3}","",text)
+
+    # remove line breaks
+    text = re.sub(r'\n','',text)
+
+    # remove puncuation
+    translator = str.maketrans('','',string.punctuation)
+    text = text.translate(translator)
+
+    # remove stop words
+    text = text.split()
+    useless_words = nltk.corpus.stopwords.words("english")
+    text_filtered = [word for word in text if not word in useless_words]
+
+    # remove numbers
+    text_filtered = [re.sub(r'\w*\d\w*','',w) for w in text_filtered]
+
+    # stem or lemmatize
+    if stem == 'Stem':
+        stemmer = PorterStemmer()
+        text_stemmed = [stemmer.stem(y) for y in text_filtered]
+    elif stem == 'Lem':
+        lem = WordNetLemmatizer()
+        text_stemmed = [lem.lemmatize(y) for y in text_filtered]
+    elif stem == 'Spacy':
+        text_filtered = nlp(' '.join(text_filtered))
+        text_stemmed = [y.lemma_ for y in text_filtered]
+    else:
+        text_stemmed = text_filtered
+
+    final_string = ' '.join(text_stemmed)
+
+    return final_string
+
+# create randomised title
+def random_title(x):
+    names = list(x.split())
+    random.shuffle(names)
+    if len(names) > 5:
+        names = names[:5]
+
+# create randomised description
+def random_description(x):
+    names = list(x.split())
+    random.shuffle(names)
+    if len(names) > 200:
+        names = names[:200]
+
+
+    return names
+
+def haversine_vectorized(user_lat,
+                         user_lon,
+                         event_lat,
+                         event_lon):
+    """
+        Calculate the great circle distance between two points
+        on the earth (specified in decimal degrees).
+        Vectorized version of the haversine distance for pandas df
+        Computes distance in kms
+    """
+
+    lat_1_rad, lon_1_rad = np.radians(user_lat), np.radians(user_lon)
+    lat_2_rad, lon_2_rad = np.radians(event_lat), np.radians(event_lon)
+    dlon = lon_2_rad - lon_1_rad
+    dlat = lat_2_rad - lat_1_rad
+
+    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat_1_rad) * np.cos(lat_2_rad) * np.sin(dlon / 2.0) ** 2
+    c = 2 * np.arcsin(np.sqrt(a))
+    return 6371 * c
